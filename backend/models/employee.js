0ba237191cc2482b4f6cf18e3a55c06ba8f25a83
laxmiChauhan98 <laxchauhan98@gmail.com>
2026-02-13 13:20:59 +0530

    First Express Server with mongoDB

---
+++ backend/models/employee.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+const employeeSchema = new mongoose.Schema({
+   
+  name: String,
+  role: String,
+  salary: Number
+});
+
+module.exports = mongoose.model("Employee", employeeSchema);
+
